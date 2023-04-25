d1c18c0a6803487dc44ed24fc85fd67e10d86704
Himanshu Kumar Gupta <ai21btech11012@iith.ac.in>
2023-04-26 01:28:15 +0530

    just changed the preprocessing program

--- preprocess.py
+++ preprocess.py
@@ -18,16 +18,20 @@ data['date_submitted'] = pd.to_datetime(data['date_submitted'])
 # data['monthly_expense'] = data['monthly_expense'].apply(lambda x: x.split('AED ')[1])
 data.replace("", np.nan, inplace=True)
 data.fillna(value='Other cinema', inplace=True)
-print(data)
+# print(data)
 single_option_columns=['gender', 'region', 'preferred_cinema', 'preferred_screen_size', 'romantic_preference', 'horror_preference', 'ott_subscription']
 multi_option_columns=['top_genres', 'preferred_time', 'watch_options', 'language_options']
 data[multi_option_columns] = data[multi_option_columns].apply(lambda x: x.str.split(','))
 mlb = MultiLabelBinarizer()
 le = LabelEncoder()
 data[single_option_columns] = data[single_option_columns].apply(le.fit_transform)
-print(data[single_option_columns])
+# print(data[single_option_columns])
 encoded_lang_options = mlb.fit_transform(data['language_options'])
-print(pd.DataFrame(encoded_lang_options, columns=mlb.classes_))
-data[multi_option_columns] = data[multi_option_columns].apply(mlb.fit_transform)
+# print(pd.DataFrame(encoded_lang_options, columns=mlb.classes_))
+
+# data[multi_option_columns] = data[multi_option_columns].apply(mlb.fit_transform)
+
 print(data[multi_option_columns])
+
+data.to_excel(excel_writer='./preprocessed_data.xlsx')
 
