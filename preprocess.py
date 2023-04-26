d453b11f300afe643671ced9d4b2a79f90a60d0a
Gunjit Mittal <gunjitmittal1@gmail.com>
2023-04-27 01:01:18 +0530

    Adding the data from mail and removing the troll data

--- preprocess.py
+++ preprocess.py
@@ -3,29 +3,24 @@ import numpy as np
 from sklearn.preprocessing import LabelEncoder, MultiLabelBinarizer
 
 # Assuming the data is stored in a CSV file named 'data.csv'
-data = pd.read_excel("data.xlsx")
-data.columns = [
-    "id", "status", "date_submitted", "gender", "age", "region", "watch_options",
-    "preferred_cinema", "language_options", "watch_duration", "weekly_duration",
-    "top_genres", "preferred_screen_size", "romantic_preference",
-    "horror_preference", "monthly_expense", "ott_subscription",
-    "preferred_time"
-]
-data['date_submitted'] = pd.to_datetime(data['date_submitted'])
+data1 = pd.read_excel("Data/data.xlsx")
+data2 = pd.read_excel("Data/data2.xlsx")
+all_clolumns = ["id", "status", "date_submitted", "gender", "age", "region", "watch_options", "preferred_cinema", "language_options", "watch_duration", "weekly_duration", "top_genres", "preferred_screen_size", "romantic_preference", "horror_preference", "monthly_expense", "ott_subscription", "preferred_time"]
+data1.columns = all_clolumns
+data2.columns = all_clolumns
 
-# data['top_genres'] = data['top_genres'].apply(lambda x: x.split(', '))
-# data['preferred_time'] = data['preferred_time'].apply(lambda x: x.split(', '))
-# data['monthly_expense'] = data['monthly_expense'].apply(lambda x: x.split('AED ')[1])
-# data.replace("", np.nan, inplace=True)
-# data.fillna(value='Other cinema', inplace=True)
+data2.drop(['id', 'status', 'date_submitted'], axis=1, inplace=True)
+data1.drop(['id', 'status', 'date_submitted'], axis=1, inplace=True)
+data = pd.concat([data1, data2], ignore_index=True)
 print(data)
 single_option_columns=['gender', 'region', 'preferred_cinema', 'preferred_screen_size', 'romantic_preference', 'horror_preference', 'ott_subscription']
 multi_option_columns=['top_genres', 'preferred_time', 'watch_options', 'language_options']
+num_columns=['age', 'watch_duration', 'weekly_duration', 'monthly_expense']
 data[multi_option_columns] = data[multi_option_columns].apply(lambda x: x.str.split(','))
 le = LabelEncoder()
 data[single_option_columns] = data[single_option_columns].apply(le.fit_transform)
 print(data[single_option_columns])
 print(data[multi_option_columns])
 
-data.to_excel(excel_writer='./preprocessed_data.xlsx')
+data.to_excel(excel_writer='preprocessed_data.xlsx')
 
